368a87540b3f025ad192d38af519fb0edd838ba9
Noemi Ruiz <112448799+noemiiruiz@users.noreply.github.com>
2022-08-31 11:32:05 -0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    { 
+    Console.WriteLine("Homework 1:");
+    string LinearEquation; 
+    LinearEquation = "Solve Z = 4X^2 + 3Y";
+    Console.WriteLine(LinearEquation);
+    double x = 2.5, y=3.3;
+    double z = 4*x*x + 3*y;
+    Console.WriteLine($"Substitute X = {x} & y = {y} to solve.");
+    Console.WriteLine($"The value of Z is = {z}");
+    
+    }
+}
